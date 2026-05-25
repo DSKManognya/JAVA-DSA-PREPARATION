8c07a67e5fe7385faca3bd6350db1619b9aaa3fe
D.S.K.Manognya <24wh1a0416@bvrithyderabad.edu.in>
2026-05-25 10:55:49 +0530

    First program in Java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+   public static void main(String[] args){// this is a function named main and it is the entry point of the program
+      System.out.println("Hello, World!"); // this line prints "Hello, World!" to the console
+      //The lines except the printing line are known as boiler plate code and is present in all Java programs.
+      // The System.out.println() method is used to print the output to the console.
+      // The println() method prints the output and also a new line character
+      // The print() method only prints the output.
+      // In Java, every application must contain a main method, which is the entry point of the program.
+
+   } 
+}
