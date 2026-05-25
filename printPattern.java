bb00726289f7a8db42f005d5a306a5a07d9f4e3b
D.S.K.Manognya <24wh1a0416@bvrithyderabad.edu.in>
2026-05-25 11:15:38 +0530

    Added explanation for using println instead of print in the code.

--- printPattern.java
+++ printPattern.java
@@ -1,5 +1,6 @@
 public class printPattern {
     public static void main(String[] args){
+        //here we have to use println as we need each line in a sepearte line, if print is used, all the stars will be on the same line
         System.out.println("****");
         System.out.println("***");
         System.out.println("**");
