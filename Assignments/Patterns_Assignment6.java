4eeafab1b89d73429f91b526544c377214d525e3
D.S.K.Manognya <24wh1a0416@bvrithyderabad.edu.in>
2026-06-09 20:23:55 +0530

    Day 09 Some more patterns

--- Assignments/Patterns_Assignment6.java
+++ Assignments/Patterns_Assignment6.java
@@ -79,5 +79,31 @@ public class Patterns_Assignment6 {
         }
         System.out.println();
     }
+    // solid rhombus
+    for(int i =1;i<=n;i++){
+        for(int j =1;j<=(n-i);j++){
+            System.out.print(" ");
+        }
+        for(int j =1;j<=n;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    //hollow rhombus
+    for(int i =1;i<=n;i++){
+       for(int j =1;j<=(n-i);j++){
+            System.out.print(" ");
+        } 
+        for(int j =1;j<=n;j++){
+            if(i==1 || i==n || j==1 || j==n){
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+        
+    }
    } 
 }
